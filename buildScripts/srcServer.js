bff509bcb913e67349f4cb97f56e3407c52b7d2d
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-11-25 14:32:47 +0100

    Fixed deprecated warnig & srcServer fixes

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -5,8 +5,10 @@ var open = require('open');
 var port = 3000;
 var app = express();
 
-app.get('/', function (req, res) {
-    res.sendfile(path.join(__dirname, '../src/index.html'));
+app.use(express.static('src'))
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/src/index.html'));
 });
 
 app.listen(port, function (err) {
