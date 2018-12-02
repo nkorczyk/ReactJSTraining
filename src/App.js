f3ee6884e76ceecb424cb3a5acd8ee1af2c150c1
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-12-02 22:16:33 +0100

    Strating work

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Movies</h1>
+        <p>List of movies</p>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style.css';
+import App from './App';
 
-const element = React.createElement(
-  "h1",
-  { className: "red_hello" },
-  "Hello World"
-);
-
-const FunctionalHello = () => <h1 className="blue_hello">Hello World</h1>;
-
-class ClassHello extends React.Component {
-  render() {
-    return <h1>Hello World</h1>;
-  }
-}
-
-class PureHello extends React.PureComponent {
-  render() {
-    return <h1>Hello World</h1>;
-  }
-}
-
-const root = React.createElement(
-  "div", {}, element, <FunctionalHello />, <ClassHello />, <PureHello />
-);
-
-ReactDOM.render(root, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
