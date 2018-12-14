a0b186ec14d7725821391aaba232162a3d2398af
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-12-14 09:59:20 +0100

    Added e2e test

--- cypress/integration/App.e2e.js
+++ cypress/integration/App.e2e.js
@@ -1,8 +1,49 @@
-describe('App E2E', () => {
-  it('should have a header', () => {
+describe('App End-to-end Test', () => {
+  it('test app', () => {
     cy.visit('/');
 
     cy.get('h4')
       .should('have.text', 'FIND YOUR MOVIE');
+
+    cy.get('.input-field')
+      .type('fifty')
+      .should('have.value', 'fifty');
+    cy.get('.input-field').type('{enter}');
+
+    cy.wait(5000);
+
+    cy.get('#moviesFound').should('contain', '3 movies found');
+    cy.screenshot('fifty-search-results');
+    cy.wait(1000);
+
+    cy.get('#genre').click();
+    cy.get('.input-field')
+      .clear()
+      .type('Adventure')
+      .should('have.value', 'Adventure');
+    cy.get('#search').click();
+
+    cy.wait(4000);
+
+    cy.get('#moviesFound').should('contain', '10 movies found');
+    cy.screenshot('adventure-genre-results');
+    cy.wait(1000);
+
+    cy.get('#title').click();
+    cy.get('.input-field')
+      .clear()
+      .type('asdrf!@')
+      .should('have.value', 'asdrf!@');
+    cy.get('.input-field').type('{enter}');
+    cy.get('#search').click();
+
+    cy.wait(4000);
+
+    cy.get('.not-found-message')
+      .should('have.text', 'No Films found');
+    cy.screenshot('no-found-results');
+    cy.wait(1000);
+
+    cy.url().should('eq', 'http://localhost/');
   });
 });
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -5,7 +5,7 @@ import CONSTANTS from '../constants/constants';
 const Results = ({ items }) => {
   return (
     <div className="bar grey lighten-1">
-      <h6 className="white-text text-darken-2 grey lighten-1 filter-bar left">{items + " " + CONSTANTS.MOVIES_FOUND}</h6>
+      <h6 id="moviesFound" className="white-text text-darken-2 grey lighten-1 filter-bar left">{items + " " + CONSTANTS.MOVIES_FOUND}</h6>
       <h6 className="white-text text-darken-2 grey lighten-1 filter-bar right">{CONSTANTS.RATING}</h6>
       <h6 className="white-text text-darken-2 grey lighten-1 filter-bar right">{CONSTANTS.RELEASE}</h6>
       <h6 className="white-text text-darken-2 grey lighten-1 filter-bar right">{CONSTANTS.SORT}</h6>
