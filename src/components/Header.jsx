a4b7a4b4cd6e0f01a6e16bed2b3d378defefecf7
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-01-10 20:01:02 +0100

    Changing highlighting while toggling Title & Genre buttons.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
 import Title from './Title';
 import CONSTANTS from '../constants/constants';
 import { loadMovies, searchBy, searchMovieChange } from '../actions/actionCreator';
@@ -15,6 +16,18 @@ class Header extends Component {
   }
 
   render() {
+    const { searchby } = this.props;
+
+    const titleClass = classNames('lighten-1 btn buttons', {
+      'red': searchby === "title",
+      'grey': searchby === "genre",
+    });
+
+    const genreClass = classNames('lighten-1 btn buttons', {
+      'red': searchby === "genre",
+      'grey': searchby === "title",
+    });
+
     return (
       <header className="header">
         <Title />
@@ -23,9 +36,9 @@ class Header extends Component {
           onChange={this.handleChange} />
         <div>
           <h6 className="white-text text-darken-2 left">{CONSTANTS.SEARCH_BY}</h6>
-          <button id="title" className="lighten-1 btn buttons"
+          <button id="title" className={titleClass}
             onClick={this.handleSearchByClick}>{CONSTANTS.TITLE}</button>
-          <button id="genre" className="lighten-1 btn buttons"
+          <button id="genre" className={genreClass}
             onClick={this.handleSearchByClick}>{CONSTANTS.GENRE}</button>
           <button id="search" className="red lighten-1 btn right"
             onClick={this.props.loadMovies}>{CONSTANTS.SEARCH}</button>
@@ -36,9 +49,9 @@ class Header extends Component {
 }
 
 const mapDispatchToProps = {
-    loadMovies,
-    searchMovieChange,
-    searchBy,
+  loadMovies,
+  searchMovieChange,
+  searchBy,
 };
 
 export { Header };
